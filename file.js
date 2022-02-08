41c159e53cd64137a62bd1ba4de6dfe94d3eeee4
DariaOni <daria.oni@outlook.com>
2022-02-08 16:50:17 +0200

    rename

--- file.js
+++ file.js
@@ -1,8 +1,12 @@
 function checkboxClick(event) {
 	console.log(event.target.checked);
+
 	let status = event.target.checked;
-	let elements = document.getElementByClassName('price');
-	if (status === false) {
-		elements = '';
+	let elements = document.getElementsByClassName('price');
+	console.log(elements);
+	if (!status) {
+		for (let i = 0; i < elements.length; i++) {
+			elements[i].innerText = '$';
+		}
 	}
 }
