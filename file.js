174e94b62b60207afa52eefd1480670823faf2d1
DariaOni <daria.oni@outlook.com>
2022-02-09 10:30:38 +0200

    added function

--- file.js
+++ file.js
@@ -2,11 +2,22 @@ function checkboxClick(event) {
 	console.log(event.target.checked);
 
 	let status = event.target.checked;
+
 	let elements = document.getElementsByClassName('price');
-	console.log(elements);
-	if (!status) {
-		for (let i = 0; i < elements.length; i++) {
-			elements[i].innerText = '$';
+	for (let i = 0; i < elements.length; i++) {
+		if (!status) {
+			elements[i].innerText = `$${+elements[i].innerText.slice(1) * 12}`;
+		} else {
+			elements[i].innerText = `$${+elements[i].innerText.slice(1) / 12}`;
+		}
+	}
+
+	let period = document.getElementsByClassName('period');
+	for (let i = 0; i < period.length; i++) {
+		if (!status) {
+			period[i].innerText = 'per user, per year';
+		} else {
+			period[i].innerText = 'per user, per month';
 		}
 	}
 }
